84ec6c1eeddf59f281071e7ae61161ec067476ef
Dark2Knight7 <ducpeti27@gmail.com>
2023-02-01 15:21:48 +0100

    feltételek létrehozása, és a nyertes konzolra való kiiratása.

--- main.py
+++ main.py
@@ -4,3 +4,15 @@
 def eredmeny():
 
     """tesztesetek"""
+    jatekosPontjai = 2
+    gepPontjai = 3
+    
+    if jatekosPontjai > 21:
+        print("Játékos vesztett.")
+    elif gepPontjai > 21:
+        print("Gép vesztett.")
+    elif 21 >= jatekosPontjai > gepPontjai:
+        print("Játékos nyert.")
+    elif 21 >= gepPontjai > jatekosPontjai:
+        print("Gép nyert.")
+
