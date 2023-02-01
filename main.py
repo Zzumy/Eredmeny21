d496e52791663d55ac70d6d771ac3ad729f2f16e
Dark2Knight7 <ducpeti27@gmail.com>
2023-02-01 15:27:42 +0100

    feltételek létrehozása a vesztésekhez.

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 """megoldás"""
 
 
-def eredmeny():
+def eredmeny(jatekosPontjai, gepPontjai):
 
-    """tesztesetek"""
-    jatekosPontjai = 2
-    gepPontjai = 3
-    
     if jatekosPontjai > 21:
         print("Játékos vesztett.")
     elif gepPontjai > 21:
         print("Gép vesztett.")
-    elif 21 >= jatekosPontjai > gepPontjai:
-        print("Játékos nyert.")
-    elif 21 >= gepPontjai > jatekosPontjai:
-        print("Gép nyert.")
 
+"""tesztesetek"""
